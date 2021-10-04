f1b827d9bb9224d7762e1bbe1780851477210297
Draco-Fahad <84138389+Draco-Fahad@users.noreply.github.com>
2021-10-05 01:28:21 +0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config                        = {}
+Config.DrawDistance           = 8
+Config.MarkerSize             = {x = 1.5, y = 1.5, z = 1.0}
+Config.MarkerColor            = {r = 102, g = 102, b = 204}
+Config.RoomMenuMarkerColor    = {r = 102, g = 204, b = 102}
+Config.MarkerType             = 1
+Config.Zones                  = {}
+Config.Properties             = {}
+Config.EnablePlayerManagement = false -- If set to true you need esx_realestateagentjob
+Config.Locale = 'en'
+Config.Webhook = 'https://discord.com/api/webhooks/894634149229518878/0lAYRViXBj47DMN3BZatE7tz8dDb1vtbAHyrRBRH3ju8Ng-SkqmUGqnhMB6hdGBKdTfK' ---change your webhook for discord logs
---
+++ sql(iam using).sql
@@ -0,0 +1,58 @@
+USE `essentialmode`;
+
+ALTER TABLE `users`
+  ADD COLUMN `last_property` VARCHAR(255) NULL
+;
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+  ('property_black_money','Argent Sale Propriété',0)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+  ('property','Propriété',0)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+  ('property','Propriété',0)
+;
+
+CREATE TABLE `owned_properties` (
+
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `rented` int(11) NOT NULL,
+  `owner` varchar(60) NOT NULL,
+
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `properties` (
+
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `entering` varchar(255) DEFAULT NULL,
+  `exit` varchar(255) DEFAULT NULL,
+  `inside` varchar(255) DEFAULT NULL,
+  `outside` varchar(255) DEFAULT NULL,
+  `ipls` varchar(255) DEFAULT '[]',
+  `gateway` varchar(255) DEFAULT NULL,
+  `is_single` int(11) DEFAULT NULL,
+  `is_room` int(11) DEFAULT NULL,
+  `is_gateway` int(11) DEFAULT NULL,
+  `room_menu` varchar(255) DEFAULT NULL,
+  `price` int(11) NOT NULL,
+
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `properties` VALUES
+  (1,'WhispymoundDrive','2677 Whispymound Drive','{\"y\":564.89,\"z\":182.959,\"x\":119.384}','{\"x\":117.347,\"y\":559.506,\"z\":183.304}','{\"y\":557.032,\"z\":183.301,\"x\":118.037}','{\"y\":567.798,\"z\":182.131,\"x\":119.249}','[]',NULL,1,1,0,'{\"x\":118.748,\"y\":566.573,\"z\":175.697}',1500000),
+  (2,'NorthConkerAvenue2045','2045 North Conker Avenue','{\"x\":372.796,\"y\":428.327,\"z\":144.685}','{\"x\":373.548,\"y\":422.982,\"z\":144.907},','{\"y\":420.075,\"z\":145.904,\"x\":372.161}','{\"x\":372.454,\"y\":432.886,\"z\":143.443}','[]',NULL,1,1,0,'{\"x\":377.349,\"y\":429.422,\"z\":137.3}',1500000),
+  (3,'RichardMajesticApt2','Richard Majestic, Apt 2','{\"y\":-379.165,\"z\":37.961,\"x\":-936.363}','{\"y\":-365.476,\"z\":113.274,\"x\":-913.097}','{\"y\":-367.637,\"z\":113.274,\"x\":-918.022}','{\"y\":-382.023,\"z\":37.961,\"x\":-943.626}','[]',NULL,1,1,0,'{\"x\":-927.554,\"y\":-377.744,\"z\":112.674}',1700000),
+  (4,'NorthConkerAvenue2044','2044 North Conker Avenue','{\"y\":440.8,\"z\":146.702,\"x\":346.964}','{\"y\":437.456,\"z\":148.394,\"x\":341.683}','{\"y\":435.626,\"z\":148.394,\"x\":339.595}','{\"x\":350.535,\"y\":443.329,\"z\":145.764}','[]',NULL,1,1,0,'{\"x\":337.726,\"y\":436.985,\"z\":140.77}',1500000),
+  (5,'WildOatsDrive','3655 Wild Oats Drive','{\"y\":502.696,\"z\":136.421,\"x\":-176.003}','{\"y\":497.817,\"z\":136.653,\"x\":-174.349}','{\"y\":495.069,\"z\":136.666,\"x\":-173.331}','{\"y\":506.412,\"z\":135.0664,\"x\":-177.927}','[]',NULL,1,1,0,'{\"x\":-174.725,\"y\":493.095,\"z\":129.043}',1500000),
+  (6,'DellPerroHeightst7','Dell Perro Heights - Apt 30',NULL,'{\"x\":-1451.562,\"y\":-523.535,\"z\":55.928}','{\"x\":-1456.02,\"y\":-519.209,\"z\":55.929}',NULL,'[]','DellPerroHeights',0,1,0,'{\"x\":-1457.026,\"y\":-530.219,\"z\":55.937}',1700000)
+;
+
